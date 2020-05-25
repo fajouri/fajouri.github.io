37e822f23fec876652a05f7a0ba8f17d6e14c8bf
HOPE <HOPE@LAPTOP-E1D9HQAG>
2020-05-25 06:50:41 -0300

    deploy new site

--- Scripts/script.js
+++ Scripts/script.js
@@ -60,7 +60,7 @@ $(document).ready(function() {
 					$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
 					if ($target.length) {
 						var targetOffset = $target.offset().top;
-						$('html,body').animate({ scrollTop: targetOffset }, 2400);
+						$('html,body').animate({ scrollTop: targetOffset }, 600);
 						return false;
 					}
 			}
